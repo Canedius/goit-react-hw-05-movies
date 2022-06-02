a120fe68879f757118936477b9c551fa0941dd72
Canedius <88346421+Canedius@users.noreply.github.com>
2022-06-02 20:55:54 +0000

    Deploying to gh-pages from @ Canedius/goit-react-hw-05-movies@bc2783abf6622305837c7d1c166a777b66ba8d1e 🚀

---
+++ static/js/NotFoundPage.401f6914.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[754],{4802:function(e,t,r){r.r(t);var a=r(184);t.default=function(e){return(0,a.jsx)("h2",{children:"\u0421\u0442\u0440\u0430\u043d\u0438\u0446\u0430 \u043d\u0435 \u043d\u0430\u0439\u0434\u0435\u043d\u0430 404"})}}}]);
+//# sourceMappingURL=NotFoundPage.401f6914.chunk.js.map
